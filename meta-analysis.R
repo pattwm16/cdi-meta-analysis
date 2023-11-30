1c32cda480079634b84e0f8be07dc68d2e37ac22
Will Patterson <wmp26@case.edu>
2023-11-30 09:24:20 -0500

    Update meta-analysis.R
    Include funnel plot code and load in risk of bias information

--- meta-analysis.R
+++ meta-analysis.R
@@ -12,6 +12,8 @@ library(tidyverse)
 
 # paths
 file_path <- "data/raw.xlsx"
+rob_path  <- "data/rob.xlsx"
+meta_path <- "data/meta_data.xlsx"
 keyv_path <- "data/study_key.csv"
 
 # data import
@@ -24,18 +26,23 @@ raw_data <- excel_sheets(file_path) %>%
   merge(., read_csv(keyv_path),                   # merge key-value pairs
         by.x="study", by.y="title") 
 
+rob <- read_excel(rob_path) %>% 
+  distinct(study, .keep_all = T) %>%
+  mutate(across(2:7, ~ as.factor(.)))
+  
+
 # what risk factors occur more than n times?
 inclusion_thresh <- 3
 
 # list risk factors
-risk_factors <- raw_data %>%
+(risk_factors <- raw_data %>%
   group_by(risk_factor) %>%
   summarise(count = n()) %>%
-  filter(count >= inclusion_thresh)
+  filter(count >= inclusion_thresh))
 
 # for example, look at PPIs and risk of HA-CDI
 ppi <- raw_data %>%
-  filter(risk_factor == "PPI") %>% # NB: can be replaced with any risk_factor
+  filter(risk_factor == "PPI") %>% 
   metagen(TE = log_or, seTE = se_log_or, 
           studlab = study_id, 
           sm = "OR", method.tau = "PM", 
@@ -43,3 +50,16 @@ ppi <- raw_data %>%
 
 # example forest plot
 ppi %>% forest(layout = "RevMan5") 
+
+# examine publication bias
+ppi %>% 
+  funnel(studlab = T, 
+               contour = c(0.9, 0.95, 0.99),
+               col.contour = c("gray75", "gray85", "gray95"))
+  
+## decorate with legend and title
+legend(x = 3, y = 0.01, 
+       legend = c("p < 0.1", "p < 0.05", "p < 0.01"),
+       fill = c("gray75", "gray85", "gray95")) 
+title("Contour-Enhanced Funnel Plot")
+
